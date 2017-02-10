f39abca8f907b25dbc976162941f097741fb1ea0
Jasmine Iwanek <jasmine@iwanek.co.uk>
2017-02-09 21:24:07 -0800

    update plugin loader

--- src/main/java/com/mcmoddev/basegems/integration/IntegrationManager.java
+++ src/main/java/com/mcmoddev/basegems/integration/IntegrationManager.java
@@ -1,11 +1,12 @@
 package com.mcmoddev.basegems.integration;
 
 import com.google.common.collect.Lists;
+import com.mcmoddev.basegems.BaseGems;
+import com.mcmoddev.basegems.integration.BaseGemsPlugin;
 import com.mcmoddev.lib.integration.IIntegration;
-import com.mcmoddev.lib.util.AnnotationChecker;
 
-import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.discovery.ASMDataTable.ASMData;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import java.util.List;
@@ -16,15 +17,20 @@ public enum IntegrationManager {
     private List<IIntegration> integrations = Lists.newArrayList();
 
     public void preInit(FMLPreInitializationEvent event) {
-        AnnotationChecker.getInstances(event.getAsmData(), BaseGemsPlugin.class, IIntegration.class).stream()
-                .forEachOrdered(integration -> {
-                    Class<? extends IIntegration> integrationClass = integration.getClass();
-                    BaseGemsPlugin plugin = integrationClass.getAnnotation(BaseGemsPlugin.class);
-                    if (Loader.isModLoaded(plugin.value())) {
-                    	FMLLog.severe("BASEGEMS: Loaded " + plugin.value());
-                        integrations.add(integration);
-                        integration.init();
-                    }
-                });
+    	for( final ASMData asmDataItem : event.getAsmData().getAll(BaseGemsPlugin.class.getCanonicalName()) ) {
+    		String modId = asmDataItem.getAnnotationInfo().get("value").toString();
+    		String className = asmDataItem.getClassName();
+    		if (Loader.isModLoaded(modId)) {
+    			IIntegration integration;
+				try {
+					integration = Class.forName(className).asSubclass(IIntegration.class).newInstance();
+					BaseGems.logger.info("BASEGEMS Loaded: "+modId);
+					integrations.add(integration);
+					integration.init();
+				} catch (final Exception e) {
+					// do nothing 
+				}
+    		}
+    	}
     }
 }
